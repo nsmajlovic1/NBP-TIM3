e54861b13480d451b9916a53954a7e39f425fb7d
Matija Kokor <matijakokorr@gmail.com>
2025-03-25 08:46:50 +0100

    Initialize Spring Boot backend project

---
+++ backend/parts-tracker/src/main/java/com/formula/parts/tracker/application/PartsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.formula.parts.tracker.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartsTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartsTrackerApplication.class, args);
+    }
+
+}
