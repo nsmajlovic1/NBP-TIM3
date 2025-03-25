e9b159542af2e78ebd07514d8ddb5d889094731b
Matija Kokor <matijakokorr@gmail.com>
2025-03-25 13:28:20 +0100

    Add data source connection data placeholders

---
+++ backend/parts-tracker/src/main/java/com/formula/parts/tracker/configuration/DataSourceConfiguration.java
@@ -0,0 +1,39 @@
+package com.formula.parts.tracker.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        final HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driverClassName);
+
+        // TODO: Remove this once the database is set up
+        config.setInitializationFailTimeout(-1);
+
+        return new HikariDataSource(config);
+    }
+
+}
