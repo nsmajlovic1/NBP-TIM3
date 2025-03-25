051b9103e980c3d8464158a23c6d7342dd3255c5
Matija Kokor <matijakokorr@gmail.com>
2025-03-25 09:03:12 +0100

    Fix test class configuration by explicitly specifying main application class

--- backend/parts-tracker/src/test/java/com/formula/parts/tracker/PartsTrackerApplicationTests.java
+++ backend/parts-tracker/src/test/java/com/formula/parts/tracker/PartsTrackerApplicationTests.java
@@ -1,9 +1,10 @@
 package com.formula.parts.tracker;
 
+import com.formula.parts.tracker.application.PartsTrackerApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PartsTrackerApplication.class)
 class PartsTrackerApplicationTests {
 
     @Test
