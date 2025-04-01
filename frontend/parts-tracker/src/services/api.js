baaa48e81df3aaee2c16785954bae09020565600
nkokor <nkokor@icloud.com>
2025-04-01 15:41:51 +0200

    add base API configuration

---
+++ frontend/parts-tracker/src/services/api.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost:8080/api", 
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const getToken = () => localStorage.getItem("token");
+
+API.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+API.interceptors.response.use(
+  (response) => response, 
+  (error) => {
+    if (error.response?.status === 401) {
+      console.warn("Token expired or invalid. Logging out...");
+
+      localStorage.removeItem("token");
+
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default API;
