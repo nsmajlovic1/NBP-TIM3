c971d70e8cbecd7354834afc91ebd10d6839b075
nkokor <nkokor@icloud.com>
2025-04-02 14:45:42 +0200

    change color theme

--- frontend/parts-tracker/src/theme.js
+++ frontend/parts-tracker/src/theme.js
@@ -3,11 +3,22 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#D32F2F", 
+      main: "#003366", 
     },
     secondary: {
-      main: "#F44336", 
+      main: "#1976D2",
     },
+    background: {
+      default: "#080808", 
+      paper: "#ffffff",
+    },
+    text: {
+      primary: "#B0B0B0", 
+      secondary: "#808080",
+    },
+  },
+  typography: {
+    fontFamily: "'Roboto', 'Arial', sans-serif", 
   },
 });
 
