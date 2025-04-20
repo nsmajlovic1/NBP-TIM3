4b41ad4732015076edfb7d248feb5febd8edc349
nkokor <nkokor@icloud.com>
2025-04-20 20:29:01 +0200

    add Add Storage button to Storages page

--- frontend/parts-tracker/src/components/Storages.js
+++ frontend/parts-tracker/src/components/Storages.js
@@ -2,11 +2,19 @@ import { useEffect, useState } from "react";
 import { getStorages } from "../services/storageService";
 import StorageMap from "./StorageMap";
 import StorageCard from "./StorageCard";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
 
 const Storages = () => {
   const [storages, setStorages] = useState([]);
   const [selectedStorage, setSelectedStorage] = useState(null);
+  const [userRole, setUserRole] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    setUserRole(storedUser?.role);
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,6 +28,10 @@ const Storages = () => {
     setSelectedStorage(storage);
   };
 
+  const handleAddStorageClick = () => {
+    console.log("Add Storage button clicked");
+  };
+
   return (
     <Box display="flex" height="100%">
       <Box flex={3} height="100%">
@@ -29,7 +41,25 @@ const Storages = () => {
           onMarkerClick={setSelectedStorage}
         />
       </Box>
+
       <Box flex={1} overflow="auto" padding={2}>
+        {["Admin", "Logistic"].includes(userRole) && (
+          <Box display="flex" justifyContent="flex-end" mb={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{
+                width: "170px",
+                fontSize: "16px",
+              }}
+              startIcon={<AddIcon />}
+              onClick={handleAddStorageClick}
+            >
+              Add Storage
+            </Button>
+          </Box>
+        )}
+
         <Box display="flex" flexDirection="column" gap={2}>
           {storages.map((storage) => (
             <StorageCard
