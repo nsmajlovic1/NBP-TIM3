0887ae2ff8ace1acf0812581f84994dde54f3371
nkokor <nkokor@icloud.com>
2025-04-20 19:24:32 +0200

    add Storages option to OptionsPanel

---
+++ frontend/parts-tracker/src/components/Storages.js
@@ -0,0 +1,15 @@
+import { Box, Typography } from "@mui/material";
+
+const Storages = () => {
+  return (
+    <Box sx={{ padding: "20px" }}>
+      <Typography variant="h4" gutterBottom>
+        Storages
+      </Typography>
+      <Typography variant="body1">
+      </Typography>
+    </Box>
+  );
+};
+
+export default Storages;
