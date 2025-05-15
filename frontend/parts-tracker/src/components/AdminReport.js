f4980c09d52e788c75a3bd54fca441a6fa383d5b
nkokor <nkokor@icloud.com>
2025-05-15 17:46:40 +0200

    separate Report view logic based on user roles

---
+++ frontend/parts-tracker/src/components/AdminReport.js
@@ -0,0 +1,16 @@
+import { Box, Typography } from "@mui/material"
+import { FaDownload } from "react-icons/fa";
+import { Button }  from "@mui/material";
+
+const AdminReport = () => {
+  return (
+    <Box>
+      <Typography variant="h4">
+        Storage Report
+      </Typography>
+
+    </Box>
+  );
+}
+
+export default AdminReport;
---
+++ frontend/parts-tracker/src/components/TeamReport.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import { Box, Typography, Button } from "@mui/material";
+
+const TeamReport = () => {
+  return (
+    <Box>
+      <Typography variant="h4">Storage Report</Typography>
+    </Box>
+  );
+};
+
+export default TeamReport;
